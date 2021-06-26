d007509d6ec980234aa8ff43aa5fe0c77d7ef6da
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-06-26 13:55:57 -0500

    init commit, used MERN templated and added in server side code

---
+++ README.md
@@ -0,0 +1,5 @@
+## Warzone Compare
+
+Website that allows users to compare their warzone stats vs their friends and pros. 
+
+Currently in development
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const unirest = require("unirest");
+
+router.get('api/:user/:platform', async (req,res)=>{
+    let {user, platform} = req.params;
+    user = user.replace('#', '%23')
+    const foreignReq = unirest("GET", `https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/${user}/${platform}`);
+    foreignReq.headers({
+        "x-rapidapi-key": process.env.CODAPIKEY,
+        "x-rapidapi-host": process.env.CODAPIHOST,
+        "useQueryString": true
+    });
+    foreignReq.end(function (data) {
+        if (data.error) {
+            res.json(data.error);
+        }
+    
+        res.json(data.body)
+    });
+});
+
+module.exports = router;
